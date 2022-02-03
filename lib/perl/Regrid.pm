fd14b61f128e9a2da97ba590322bd691de1b1d2a
Adam Allgood <avram.meir@gmail.com>
2022-02-03 16:20:07 -0500

    Add POD documentation

--- lib/perl/Regrid.pm
+++ lib/perl/Regrid.pm
@@ -4,6 +4,49 @@ package Regrid;
 
 =pod
 
+=head1 NAME
+
+Regrid - Regrid gridded binary data to a 1/8th degree format required by grids-to-climdivs
+
+=head1 SYNOPSIS
+
+ use Regrid qw(regrid);
+ 
+ my $binary_data_file    = "/some/path/to/data/file"
+ my $grib2_template_file = "/some/path/to/grib2/template/file"
+ my $output_file         = "/where/you/want/output/to/go"
+ 
+ my $error = regrid($grib2_template_file,$binary_data_file,$output_file);
+
+=head1 DESCRIPTION
+
+The Regrid package is part of the grids-to-climdivs project. It provides the functionality to convert unformatted binary gridded data files to a special 1/8th degree grid used by the project to map gridpoints onto climate divisions.
+
+=head1 REQUIREMENTS
+
+=over 3
+
+=item * L<wgrib2|https://www.cpc.ncep.noaa.gov/products/wesley/wgrib2/> must be installed on your system
+
+=back
+
+=head1 METHODS
+
+Regrid.pm provides one method used to convert binary data to the format needed by grids-to-climdivs to produce climate divisions data.
+
+=head2 regrid
+
+ my $error = regrid($template,$input,$output,[$rpn]);
+
+Given a grib2 "template" file (filename set to $template) with dimensions matching an unformatted binary data file (filename set to $input), writes unformatted binary data regridded to 1/8th degree lat/lon to the filename passed in as $output. If $output exists it will be overwritten.
+
+The template, input, and output arguments are required. A fourth option, "rpn" can be supplied. The method uses wgrib2 to regrid the binary data, and wgrib2 can take a -rpn (reverse Polish notation) argument useful for things like unit conversions. The user can pass the reverse Polish notation conversion information via this argument.
+
+=head1 AUTHOR
+
+Adam Allgood
+
+This documentation was last updated on: 03FEB2022
 
 =cut
 
