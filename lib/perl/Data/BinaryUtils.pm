d97b3fe715808b8225009e61eb297901a36dbfeb
Adam Allgood <avram.meir@gmail.com>
2022-03-30 21:58:34 -0400

    Add regrid function code

--- lib/perl/Data/BinaryUtils.pm
+++ lib/perl/Data/BinaryUtils.pm
@@ -33,6 +33,7 @@ This documentation was last updated on:29MAR2022
 use strict;
 use warnings;
 use Carp qw(carp cluck croak confess);
+use Scalar::Util qw(looks_like_number reftype);
 require File::Temp;
 use File::Temp ();
 use File::Temp qw(:seekable);
@@ -55,8 +56,52 @@ sub flipbytes {
 }
 
 sub regrid {
+	confess "Three arguments required" unless(@_ >= 2);
+	my $config = shift;
+	my $map    = shift;
+	my $output = shift;
+	if(reftype($config)) { confess "Invalid config argument" unless reftype($config) eq 'HASH'; }
+	else                 { confess "Invalid config argument"; }
+	confess "Config argument is missing parameters" unless(exists $config->{"input.template"} and exists $config->{"input.file"} and exits $config->{"input.headers"} and exists $config->{"input.missing"} and exists $config->{"output.rpn"});
+	my $template  = $config->{"input.template"};
+	my $input     = $config->{"input.file"};
+	my $header    = $config->{"input.headers"};
+	my $missing   = $config->{"input.missing"};
+	my $rpn       = $config->{"output.rpn"};
+	confess "$template must be an existing file" unless(-s $template);
+	confess "$input must be an existing file"    unless(-s $input);
+	if($rpn) { $rpn = " -rpn \"$rpn\""; }
+	else     { $rpn = "";               }
+	confess "$map is invalid" unless($map eq 'CONUS' or $map eq 'AK-HI');
 
+	# --- Import binary dataset into grib2 format ---
+	
+	my $work_dir  = File::Temp->newdir();
+	my $grib_orig = File::Temp->new(DIR => $work_dir);
+	my $grib_orig_fn = $grib_orig->filename;
+	my $error        = system("$wgrib2 -d 1 $template -import_ieee $input -$header -$byteorder -undefine_val $missing$rpn -set_date 19710101 -set_grib_type j -set_scaling -1 0 -grib_out $grib_orig_fn > /dev/null");
+	if($error) { confess "Could not import $input into grib2 format"; }
 
+	# --- Regrid data to the map grid ---
+
+	my $grib_rg      = File::Temp->new(DIR => $work_dir);
+	my $grib_rg_fn   = $grib_rg->filename;
+
+	if($map eq 'CONUS') {
+		$error = system("$wgrib2 $grib_orig_fn -set_grib_type jpeg -new_grid_winds earth -new_grid latlon 230:601:0.125 20:241:0.125 $grib_rg_fn > /dev/null");
+		if($error) { confess "Could not regrid grib2 version of $input"; }
+	}
+	else                {
+		$error = system("$wgrib2 $grib_orig_fn -set_grib_type jpeg -new_grid_winds earth -new_grid latlon 0.083333:2160:0.166667 -89.916667:1080:0.166667 $grib_rg_fn > /dev/null");
+		if($error) { confess "Could not regrid grib2 version of $input"; }
+	}
+
+	# --- Export regridded data into unformatted binary format ---
+
+	$error = system("$wgrib2 $grib_rg_fn -no_header -bin $output > /dev/null");
+	if($error) { confess "Could not create binary file $output"; }
+
+	return 0;
 }
 
 1;
