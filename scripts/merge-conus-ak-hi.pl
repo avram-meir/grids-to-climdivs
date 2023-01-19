fe911ed9b8457d787eb00f5a5f04a79547f1ebfb
Adam Allgood <adam.allgood@noaa.gov>
2023-01-19 20:12:27 +0000

    Finished coding script - needs testing

--- scripts/merge-conus-ak-hi.pl
+++ scripts/merge-conus-ak-hi.pl
@@ -126,182 +126,73 @@ if($opts_failed) {
 
 # --- Create output directory if needed ---
 
-my($output_name,$output_path,$output_suffix) = fileparse($output_file, qr/\.[^.]*/); }
+my($output_name,$output_path,$output_suffix) = fileparse($output_file, qr/\.[^.]*/);
 unless(-d $output_path) { mkpath($output_path) or die "Could not create directory $output_path - $! - exiting"; }
+
+# --- Get climdivs ---
+
+my $climdivs_file = $script_path."/../lib/climdivs/climdivs363.txt";
+open(CLIMDIVS,'<',$climdivs_file) or die "Could not open $climdivs_file for readiing - $! - exiting";
+my @climdivs_contents = <CLIMDIVS>; chomp @climdivs_contents;
+close(CLIMDIVS);
+my @climdivs;
+
+foreach my $line (@climdivs_contents) { 
+    my($num,$climdiv,$name) = split('|',$line);
+    push(@climdivs,$climdiv);
+}
 
 # --- Get input data ---
 
+open(ALASKA,'<',$alaska_file) or die "Could not open $alaska_file for reading - $! - exiting";
+open(CONUS,'<',$conus_file)   or die "Could not open $conus_file for reading - $! - exiting";
+open(HAWAII,'<',$hawaii_file) or die "Could not open $hawaii_file for reading - $! - exiting";
 
+my @alaska_contents = <ALASKA>; chomp @alaska_contents;
+close(ALASKA);
+my @conus_contents  = <CONUS>;  chomp @conus_contents;
+close(CONUS);
+my @hawaii_contents = <HAWAII>; chomp @hawaii_contents;
+close(HAWAII);
 
+shift @alaska_contents;
+my $header = shift @conus_contents;
+shift @hawaii_contents;
 
+my(%alaska,%conus,%hawaii);
 
-# --- Pull information from the configuration file ---
-
-my $config = Config::Simple->new($config_file)->vars();
-
-# --- List of allowed variables that can be in the config file ---
-
-my %allowed_vars = (
-        APP_PATH => "$script_path..",
-        DATA_IN  => $ENV{DATA_IN},
-        DATA_OUT => $ENV{DATA_OUT},
-);
-
-# --- Parse variables and date wildcards in the config file params ---
-
-foreach my $param (keys %$config) {
-
-	if(ref($config->{$param}) eq 'ARRAY') {
-
-		for(my $i=0; $i<scalar(@{$config->{$param}}); $i++) {
-			$config->{$param}[$i] = parse_param($config->{$param}[$i],$day);
-		}
-
-	}
-	else {
-		$config->{$param} = parse_param($config->{$param},$day);
-	}
-
+foreach my $line (@alaska_contents) {
+    my($cd,$val) = split(',',$line);
+    $alaska{$cd} = $val;
 }
 
-# --- QC the config file params ---
-
-unless(-s $config->{'input.file'}) {
-	die "The input.file ".$config->{'input.file'}." in $config_file does not exist - exiting";
+foreach my $line (@conus_contents) {
+    my($cd,$val) = split(',',$line);
+    $conus{$cd} = $val;
 }
 
-unless($config->{'input.ngrids'} eq int($config->{'input.ngrids'}) and $config->{'input.ngrids'} > 0) {
-	die "The input.ngrids param in $config_file is invalid - exiting";
+foreach my $line (@hawaii_contents) {
+    my($cd,$val) = split(',',$line);
+    $hawaii{$cd} = $val;
 }
 
-# Bugfix - the code assumes that output.grids, output.files, and output.descriptions are 
-# all array refs, but if ngrids=1 then these will be scalars. So let's just force them to 
-# always be array refs.
+# --- Output merged climdivs data ---
 
-$config->{'output.grids'}        = [ $config->{'output.grids'} ]        if ref($config->{'output.grids'}) ne 'ARRAY';
-$config->{'output.files'}        = [ $config->{'output.files'} ]        if ref($config->{'output.files'}) ne 'ARRAY';
-$config->{'output.descriptions'} = [ $config->{'output.descriptions'} ] if ref($config->{'output.descriptions'}) ne 'ARRAY';
+open(OUTPUT,'>',$output_file) or die "Could not open $output_file for writing - $! - exiting";
+print OUTPUT "$header\n";
 
-unless(scalar(@{$config->{'output.grids'}}) <= $config->{'input.ngrids'}) {
-    die "The number of items in output.grids exceeds the value of input.ngrids in $config_file - exiting";
+foreach my $climdiv (@climdivs) {
+    my $val = -999;
+
+    if($climdiv =~ /AK/ and exists $alaska{$climdiv} and abs($alaska{$climdiv}) < 100000)    { $val = $alaska{$climdiv}; }
+    elsif($climdiv =~ /HI/ and exists $hawaii{$climdiv} and abs($hawaii{$climdiv}) < 100000) { $val = $hawaii{$climdiv}; }
+    elsif(exists $conus{$climdiv} and abs($conus{$climdiv}) < 100000) { $val = $conus{$climdiv}; }
+
+    print OUTPUT join(',',$climdiv,$val)."\n";
 }
 
-unless(scalar(@{$config->{'output.grids'}}) == scalar(@{$config->{'output.files'}}) and scalar(@{$config->{'output.grids'}}) == scalar(@{$config->{'output.descriptions'}})) {
-    die "The number of items in output.grids, output.files, and output.descriptions do not match in $config_file - exiting";
-}
+close(OUTPUT);
 
-# --- Split the input file into ngrids pieces ---
-
-# This is assumes the input file is pure unformatted binary so that it'll divide evenly into input_ngrids pieces
-
-my $npieces     = $config->{'input.ngrids'};
-my $binary_file = $config->{'input.file'};
-my $split_dir   = File::Temp->newdir();
-
-open(SPLIT, "split -n $npieces --verbose $binary_file $split_dir/input 2>&1 |") or die "Could not split $binary_file into $npieces pieces for processing - exiting";
-
-my @input_files;
-
-while (<SPLIT>) {
-
-	if ( /^creating file (.*)$/ ) {
-		my $split_file = $1;
-		$split_file    =~ s/[\p{Pi}\p{Pf}'"]//g;  # Remove every type of quotation mark
-		$split_file    =~ s/[^[:ascii:]]//g;      # Remove non-ascii characters
-		push(@input_files, $split_file);
-	}
-	else {
-		warn "Warning: this output line from split was not parsed: $_";
-	}
-
-}
-
-close(SPLIT);
-
-# --- Loop through the input pieces and create climdivs data for the requested grids ---
-
-my $counter  = 0;
-my $work_dir = File::Temp->newdir();
-
-foreach my $og (@{$config->{'output.grids'}}) {
-	print "Converting grid $og of $npieces in $binary_file to climate divisions\n";
-
-	my $input_fn   = $input_files[$og-1];
-	my $conus_fh   = File::Temp->new(DIR => $work_dir);
-	my $akhi_fh    = File::Temp->new(DIR => $work_dir);
-
-	# --- Regrid the input data to match the CONUS map if needed ---
-	
-	my $conus_fn = $input_fn;
-
-	if($config->{'input.rgconus'}) {
-		$conus_fn = $conus_fh->filename;
-		regrid($input_fn,$config,'CONUS',$conus_fn);
-	}
-
-	# --- Regrid the input data to match the AK-HI map if needed ---
-	
-	my $akhi_fn = $input_fn;
-
-	if($config->{'input.rgakhi'}) {
-		$akhi_fn = $akhi_fh->filename;
-		regrid($input_fn,$config,'AK-HI',$akhi_fn);
-	}
-
-	# --- Get climate divisions data from the gridded data ---
-
-	open(CONUS,'<',$conus_fn) or die "Could not open $conus_fn for reading - $! - exiting";
-	binmode(CONUS);
-	my $conus_grid = join('',<CONUS>);
-	close(CONUS);
-	open(AKHI,'<',$akhi_fn) or die "Could not open $akhi_fn for reading - $! - exiting";
-	binmode(AKHI);
-	my $akhi_grid  = join('',<AKHI>);
-	close(AKHI);
-	my $climdivs = GridToClimdivs->new();
-	$climdivs->set_missing($config->{'input.missing'});
-	$climdivs->set_conus_data(\$conus_grid);
-	$climdivs->set_akhi_data(\$akhi_grid);
-	my $climdivs_data = $climdivs->get_data();
-
-	# --- Write climate divisions data to file ---
-	
-	my $output_file = "$output/".$config->{'output.files'}[$counter];
-	my($output_name,$output_path,$output_suffix) = fileparse($output_file, qr/\.[^.]*/);
-	unless(-d $output_path) { mkpath $output_path or die "Could not create directory $output_path - $! - exiting"; }
-	open(OUTPUT,'>',$output_file) or die "Could not open $output_file for writing - $! - exiting";
-	print OUTPUT join('|','STCD',$config->{'output.descriptions'}[$counter])."\n";
-	my @stcd = $climdivs->get_climdivs_list();
-
-	foreach my $stcd (@stcd) {
-		my $value = $climdivs_data->{$stcd};
-		if($config->{'output.rpn'}) { $value = rpn_calc(join(':',$value,$config->{'output.rpn'}),':'); }
-		print OUTPUT join('|',$stcd,sprintf("%.3f",$value))."\n";
-	}
-
-	print "$output_file written!\n";
-	$counter++;
-}
-
-# --- End of script ---
-
-sub parse_param {
-	my $param = shift;
-	my $day   = shift;
-
-	# --- List of allowed variables that can be in the config file ---
-
-	my %allowed_vars = (
-		APP_PATH => "$script_path..",
-		DATA_IN  => $ENV{DATA_IN},
-		DATA_OUT => $ENV{DATA_OUT},
-	);
-
-	my $param_parsed = $param;
-	$param_parsed    =~ s/\$(\w+)/exists $allowed_vars{$1} ? $allowed_vars{$1} : 'illegal000BLORT000illegal'/eg;
-	if($param_parsed =~ /illegal000BLORT000illegal/) { die "Illegal variable found in $param"; }
-	return $day->printf($param_parsed);
-}
-
+print "\n$output_file written!\n";
 exit 0;
 
